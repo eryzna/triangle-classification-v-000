68560e6f490cecc8c3d682a6a0041d0c553fafd3
Elana Ryznar <eryzna@gmail.com>
2018-07-26 16:14:01 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,15 @@
 class Triangle
+
+  def initialize (length_1, length_2, length_3)
+    @length_1=length_1
+    @length_2=length_2
+    @length_3=length_3
+  end
+
+  def kind
+    if length_1==length_2 && length_3
+      :equilateral
+    end
+  end    
   # write code here
 end

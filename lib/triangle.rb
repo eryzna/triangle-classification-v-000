5ace7d49b3c3bdf6996062af15ab83b6259ce011
Elana Ryznar <eryzna@gmail.com>
2018-07-26 16:26:56 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,7 @@ class Triangle
   def kind
     if length_1==length_2 && length_3
       :equilateral
-    elsif length_1 != length_2 && length_3
+    elsif length_1 != length_2 || length_3
       :scalene
     elsif length_2==length_3 || length_1==length_2 || length_1==length_3
       :isosceles

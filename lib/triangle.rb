68ad38f276a3fad329cd74e58e4116c3ddb08420
Elana Ryznar <eryzna@gmail.com>
2018-07-26 17:16:48 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,12 +15,17 @@ class Triangle
       #:scalene
     elsif length_2==length_3 || length_1==length_2 || length_1==length_3
       :isosceles
-    elsif length_1 && length_2 && length_3 == 0
-      raise TriangleError
     else
       :scalene
     end
   end
+
+  def check_triangle
+    real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+    [a, b, c].each { |s| real_triangle << false if s <= 0 }
+    raise TriangleError if real_triangle.include?(false)
+  end
+    
 
   class TriangleError < StandardError
 
